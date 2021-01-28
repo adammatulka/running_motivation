70ce41953b9f24aa046c8fc50430c67d3fac9025
Adam Matulka <matulka.adam@gmail.com>
2021-01-28 15:03:51 +0100

    init testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,67 @@
-import React, {useState} from 'react';
-import moment from 'moment';
+import React, {useRef, useState} from 'react';
+//import moment from 'moment';   
 
 export const Main = () => {
 
-    const [pace, setPace] = useState('00:00 min/km');
-    const [runtime, setRuntime] = useState("Time: 00:12:34");
+    const [pace, setPace] = useState();
+    const [isRunning, setIsRunning] = useState(false);
+    const [isPaused, setIsPaused] = useState(false);
+    const [runtime, setRuntime] = useState(3555);
+    const countRef = useRef(null) 
+     
+    const handleStart = (e) => {
+        e.preventDefault(); 
+        setIsRunning(true)
+        setIsPaused(false)
+        countRef.current = setInterval(() => {
+            setRuntime((runtime) => runtime + 1)
+        }, 1000)
+    }    
+
+    const handleStop = (e) => {
+        e.preventDefault(); 
+        clearInterval(countRef.current)
+        setIsPaused(true)
+    }
+ /*   const handleStart = (e) =>{
+        e.preventDefault(); 
+        setRuntime(moment().format("h:mm:ss:SS"));
+        setInterval(function(){ setRuntime(moment().format("h:mm:ss:SS")); },100);
+    }    
+
+    const handleStop = (e) =>{
+        e.preventDefault(); 
+        setRuntime();
+    }
+*/
+    const formatTime = () => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`
+  }
+
+
+    const handleAdd = () =>  {
+
+    }
+
+    const handleSub = () =>  {
+        
+    }
 
     return(
         <div>
-            <button>-</button>
+            <button onClick={handleSub}>-</button>
                 <label>
                     <input type="text" value={ pace } onChange={(e) => setPace(e.target.value)} />
                 </label>
-            <button>+</button>
+            <button onClick={handleAdd}>+</button>
 
-            <button>Start</button>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
             <p>
                 <strong>{runtime}</strong>
             </p>
